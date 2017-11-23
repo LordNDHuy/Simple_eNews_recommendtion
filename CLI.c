406621b35f9e2b391b392e9ec7717640151d28fe
Timothy <30655730+tommyjohn1001@users.noreply.github.com>
2017-11-23 19:15:14 +0700

    Add files via upload
    text.txt : file storing e_news data
    CLI.h + CLI.c : files managing command line interface
    => call FileReader_E_news() to extract data to array E_news_arr

---
+++ CLI.c
@@ -0,0 +1,86 @@
+/*
+ * CLI.c
+ *
+ *  Created on: Nov 22, 2017
+ *      Author: Tommy
+ */
+
+#include "CLI.h"
+#include "../funcs.h"
+
+//interface
+void header(){
+    printf("******************************************* \n");
+    printf("*                                         * \n");
+    printf("*    *****       **    * ***** **   **    * \n");
+    printf("*    *           * *   * *     * * * *    * \n");
+    printf("*    ***** ----  *  *  * ***** *  *  *    * \n");
+    printf("*    *           *   * * *     *     *    * \n");
+    printf("*    *****       *    ** ***** *     *    * \n");
+    printf("*                                         * \n");
+    printf("******************************************* \n");
+}
+
+//file manipulation
+size_t e_news_num = 0;
+struct e_news struct_dissolve(char *input){
+	struct e_news temp;
+								//hold position for line scanning
+	short space = 0;
+
+	unsigned short ID_position = 0;					//hold position for ID input
+	unsigned short date_position = 0;
+	size_t fullcontent_position = 0;
+	unsigned short taglistWord_position = 0;
+	size_t taglist = 0;
+
+	for (size_t position = 0; input[position] != '\0'; position++){
+		if (input[position] == ' ') {space++; continue;}
+
+		switch (space){
+		case 0: {									//ID scanning
+			temp.ID[ID_position] = input[position];
+			ID_position++; break;}
+		case 1: {									// category scanning
+			temp.category = input[position] - '0';
+			break;}
+		case 2:{									// day, month, year scanning
+			temp.pubDate[date_position] = input[position];
+			date_position++; break;}
+		case 3:{									// full content scanning
+			temp.full_content[fullcontent_position] = input[position];
+			fullcontent_position++; break;}
+		case 4:{									//taglist scanning
+			if (input[position] == ';') {taglistWord_position = 0; taglist++; continue;}
+			temp.tag_list[taglist][taglistWord_position] = input[position];
+			taglistWord_position++; break;}
+		}
+	}
+	return temp;
+}
+
+void FileReader_E_news(){
+	FILE *file;
+	char * line = NULL;
+	size_t len = 0;
+	size_t position = 0;
+
+	file = fopen("./resources/text.txt", "r");
+	if (file == NULL) {
+		printf("File could not be found! ");
+		exit(-1);
+	}
+
+	while (getline(&line, &len, file) != EOF) {
+		if (e_news_num == 0) {
+			e_news_num = atoi(line);
+			E_news_arr = (struct e_news*) malloc (sizeof(struct e_news)*e_news_num);
+			continue;
+		}
+
+		E_news_arr[position] = struct_dissolve(line);
+		position++;
+	}
+
+	fclose(file);
+}
---
+++ CLI.h
@@ -0,0 +1,17 @@
+/*
+ * CLI.h
+ *
+ *  Created on: Nov 23, 2017
+ *      Author: root
+ */
+
+#ifndef IO_CLI_H_
+#define IO_CLI_H_
+
+extern void header(void);
+
+// file manipulation
+extern void FileReader_E_news(void);
+extern void FileReader_query(void);
+
+#endif /* IO_CLI_H_ */
